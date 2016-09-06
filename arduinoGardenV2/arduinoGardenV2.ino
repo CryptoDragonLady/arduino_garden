5dd1abf7c60d2cd301743b3b858b866bdeb2684d
Andrew Megaris <andrew.megaris@gmail.com>
2016-09-06 14:11:29 -0500

    almost done

--- arduinoGardenV2/arduinoGardenV2.ino
+++ arduinoGardenV2/arduinoGardenV2.ino
@@ -20,11 +20,11 @@ int potThreeSoilSensor = 2;
 int potFourSoilSensor = 3;
 LiquidCrystal_I2C lcd(0x3f,20,4); // analog pins 4 and 5
 //End Analog Pins//////
-int one_second = 1000;
+int oneSecond = 1000;
 int moisturePotOne,moisturePotTwo,moisturePotThree,moisturePotFour = 1000;
-int dayTime, nightTime;
-boolean lightOn = true;
-
+int lightTime = 0;
+int soilCheckTimer = 0;
+boolean lightOn = false;
 ////END GLOBAL VARIABLES////
 
 
@@ -38,43 +38,48 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   light_check_time();
- moisture_check_pot_one();
-//  moisture_check_pot_two();
-  delay(one_second);
+  check_soil_moisture();
+  delay(oneSecond);
 }
 
 
 /////////////MOISTURE SENSOR STUFf/////
 void check_soil_moisture(){
-  //pot1 is pin 0, pot 2 pin 1....
-  for(int x  = 1; x <= 15; x++){
-    moisturePotOne += analogRead(potOneSoilSensor);
-    moisturePotTwo += analogRead(potTwoSoilSensor);
-    moisturePotThree += analogRead(potThreeSoilSensor);
-    moisturePotFour += analogRead(potFourSoilSensor);
+  if(check_soil_moisture == 3600){
+    
+    for(int x  = 1; x <= 15; x++){
+      moisturePotOne += analogRead(potOneSoilSensor);
+      moisturePotTwo += analogRead(potTwoSoilSensor);
+      moisturePotThree += analogRead(potThreeSoilSensor);
+      moisturePotFour += analogRead(potFourSoilSensor);
+    }
+    moisturePotOne =   (moisturePotOne / 15);
+    moisturePotTwo =   (moisturePotTwo / 15);
+    moisturePotThree = (moisturePotThree / 15);
+    moisturePotFour =  (moisturepotFour / 15);  
+  
+    Serial.print("p1: ");
+    Serial.println(moisturePotOne);
+    Serial.print("p2: ");
+    Serial.println(moisturePotTwo);
+    Serial.print("p3: ");
+    Serial.println(moisturePotThree);
+    Serial.print("p4: ");
+    Serial.println(moisturePotFour;
+  
+    if(moisturePotOne <= 700)
+      waterPot(1);
+    if(moisturePotTwo <= 700)
+      waterPot(2);
+    if(moisturePotThree <= 700)
+      waterPot(3);
+    if(moisturePotFour <= 700)
+      waterPot(4);
+      
+    check_soil_moisture = 0;
   }
-  moisturePotOne =   (moisturePotOne / 15);
-  moisturePotTwo =   (moisturePotTwo / 15);
-  moisturePotThree = (moisturePotThree / 15);
-  moisturePotFour =  (moisturepotFour / 15);  
-  
-  Serial.print("p1: ");
-  Serial.println(moisturePotOne);
-  Serial.print("p2: ");
-  Serial.println(moisturePotTwo);
-  Serial.print("p3: ");
-  Serial.println(moisturePotThree);
-  Serial.print("p4: ");
-  Serial.println(moisturePotFour;
-  
-  if(moisturePotOne <= 700)
-    waterPot(1);
-  if(moisturePotTwo <= 700)
-    waterPot(2);
-  if(moisturePotThree <= 700)
-    waterPot(3);
-  if(moisturePotFour <= 700)
-    waterPot(4);
+  else
+    check_soil_moisture++;
     
 }
 /// END MOISTURE SENSOR STUFF//////////
@@ -82,39 +87,21 @@ void check_soil_moisture(){
 /////////////LIGHT STUFF///////////////
 void light_setup(){
   pinMode(relayOne, OUTPUT);
-  digitalWrite(relayOne,LOW);
-  dayTime = 0;
-}
-
-void light_toggle(){
-  if(lightOn){
-    digitalWrite(relayOne,HIGH);
-    lightOn = false;
-    nightTime = 0;
-  }
-  else{
-    digitalWrite(relayOne,LOW);
-    lightOn = true;
-    dayTime = 0;
-  }
+  digitalWrite(relayOne,HIGH);
+  lightTime = 0;
 }
 
 void light_check_time(){
-  if(lightOn){
-    if(dayTime < (60*60)){
-      dayTime++;
-    }
-    else{
-      light_toggle();  
-    }  
+  if(lightTime <= 60){
+    digitalWrite(relayOne,HIGH);
+    lightTime++;
+  }
+  else if(lightTime > 60 && lightTime < 120){
+    digitalWrite(relayOne,LOW);
+    lightTime++;
   }
   else{
-    if(nightTime < (60* 5)){
-      nightTime++;  
-    }
-    else{
-      light_toggle();  
-    }  
+    lightTime = 0;
   }
 }
 /////////////END LIGHT STUFF///////////
@@ -133,25 +120,25 @@ void valve_setup(){
 void waterPot(int potToWater){
   if(potToWater == '1'){
     digitalWrite(relayFive, LOW);
-    delay(one_second * 15);
+    delay(oneSecond * 15);
     digitalWrite(relayFive, HIGH);
     Serial.println("pot one watered);
   }
     if(potToWater == '2'){
     digitalWrite(relaySix, LOW);
-    delay(one_second * 15);
+    delay(oneSecond * 15);
     digitalWrite(relaySix, HIGH);
     Serial.println("pot two watered);
   }
     if(potToWater == '3'){
     digitalWrite(relaySeven, LOW);
-    delay(one_second * 15);
+    delay(oneSecond * 15);
     digitalWrite(relaySeven, HIGH);
     Serial.println("pot three watered);
   }
     if(potToWater == '4'){
     digitalWrite(relayEight, LOW);
-    delay(one_second * 15);
+    delay(oneSecond * 15);
     digitalWrite(relayEight, HIGH);
     Serial.println("pot four watered);
   }
