842595e8889411126fcdff34283c766dbb816695
Andrew Megaris <andrew.megaris@gmail.com>
2016-09-06 13:56:24 -0500

    cleaning up programming

---
+++ arduinoGardenV2/arduinoGardenV2.ino
@@ -0,0 +1,173 @@
+//INCLUDES//////////////
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+/////END OF INCLUDES////
+
+//GLOBAL VARIABLES//////
+
+//Digital Pin///////////
+int relayOne = 7;//light relay
+int relayFive = 8;//pot one valve
+int relaySix = 9;//pot two valve
+int relaySeven = 10;//pot three valve
+int relayEight = 11;//pot four valve
+//End Digital Pin//////
+
+//Analog Pins//////////
+int potOneSoilSensor = 0;
+int potTwoSoilSensor = 1;
+int potThreeSoilSensor = 2;
+int potFourSoilSensor = 3;
+LiquidCrystal_I2C lcd(0x3f,20,4); // analog pins 4 and 5
+//End Analog Pins//////
+int one_second = 1000;
+int moisturePotOne,moisturePotTwo,moisturePotThree,moisturePotFour = 1000;
+int dayTime, nightTime;
+boolean lightOn = true;
+
+////END GLOBAL VARIABLES////
+
+
+void setup() {
+  Serial.begin(9600);
+  light_setup();
+  display_setup();
+  valve_setup();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  light_check_time();
+ moisture_check_pot_one();
+//  moisture_check_pot_two();
+  delay(one_second);
+}
+
+
+/////////////MOISTURE SENSOR STUFf/////
+void check_soil_moisture(){
+  //pot1 is pin 0, pot 2 pin 1....
+  for(int x  = 1; x <= 15; x++){
+    moisturePotOne += analogRead(potOneSoilSensor);
+    moisturePotTwo += analogRead(potTwoSoilSensor);
+    moisturePotThree += analogRead(potThreeSoilSensor);
+    moisturePotFour += analogRead(potFourSoilSensor);
+  }
+  moisturePotOne =   (moisturePotOne / 15);
+  moisturePotTwo =   (moisturePotTwo / 15);
+  moisturePotThree = (moisturePotThree / 15);
+  moisturePotFour =  (moisturepotFour / 15);  
+  
+  Serial.print("p1: ");
+  Serial.println(moisturePotOne);
+  Serial.print("p2: ");
+  Serial.println(moisturePotTwo);
+  Serial.print("p3: ");
+  Serial.println(moisturePotThree);
+  Serial.print("p4: ");
+  Serial.println(moisturePotFour;
+  
+  if(moisturePotOne <= 700)
+    waterPot(1);
+  if(moisturePotTwo <= 700)
+    waterPot(2);
+  if(moisturePotThree <= 700)
+    waterPot(3);
+  if(moisturePotFour <= 700)
+    waterPot(4);
+    
+}
+/// END MOISTURE SENSOR STUFF//////////
+
+/////////////LIGHT STUFF///////////////
+void light_setup(){
+  pinMode(relayOne, OUTPUT);
+  digitalWrite(relayOne,LOW);
+  dayTime = 0;
+}
+
+void light_toggle(){
+  if(lightOn){
+    digitalWrite(relayOne,HIGH);
+    lightOn = false;
+    nightTime = 0;
+  }
+  else{
+    digitalWrite(relayOne,LOW);
+    lightOn = true;
+    dayTime = 0;
+  }
+}
+
+void light_check_time(){
+  if(lightOn){
+    if(dayTime < (60*60)){
+      dayTime++;
+    }
+    else{
+      light_toggle();  
+    }  
+  }
+  else{
+    if(nightTime < (60* 5)){
+      nightTime++;  
+    }
+    else{
+      light_toggle();  
+    }  
+  }
+}
+/////////////END LIGHT STUFF///////////
+
+////////////VALVE STUFF////////////////
+void valve_setup(){
+  pinMode(relayFive,OUTPUT);
+  pinMode(relaySix,OUTPUT);
+  pinMode(relaySeven,OUTPUT);
+  pinMode(relayEight,OUTPUT);
+  digitalWrite(relayFive,HIGH);
+  digitalWrite(relaySix,HIGH);
+  digitalWrite(relaySeven,HIGH);
+  digitalWrite(relayEight,HIGH);
+}
+void waterPot(int potToWater){
+  if(potToWater == '1'){
+    digitalWrite(relayFive, LOW);
+    delay(one_second * 15);
+    digitalWrite(relayFive, HIGH);
+    Serial.println("pot one watered);
+  }
+    if(potToWater == '2'){
+    digitalWrite(relaySix, LOW);
+    delay(one_second * 15);
+    digitalWrite(relaySix, HIGH);
+    Serial.println("pot two watered);
+  }
+    if(potToWater == '3'){
+    digitalWrite(relaySeven, LOW);
+    delay(one_second * 15);
+    digitalWrite(relaySeven, HIGH);
+    Serial.println("pot three watered);
+  }
+    if(potToWater == '4'){
+    digitalWrite(relayEight, LOW);
+    delay(one_second * 15);
+    digitalWrite(relayEight, HIGH);
+    Serial.println("pot four watered);
+  }
+}
+///////////END VALVE STUFF/////////////
+
+//////////// DISPLAY STUFF ///////////
+void display_setup(){
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("LCD BOOT SUCCESS");
+  lcd.setCursor(0,1);
+  lcd.print("Welcome Master! =)");
+}
+
+///////////END DISPLAY STUFF//////////
+
+
